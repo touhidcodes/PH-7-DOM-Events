33b909ed84c137c0f08d38ac56a4fdb78e17ff27
Touhidur Zaman <touhidcodes@gmail.com>
2023-02-11 13:17:35 +0600

    event-method

--- js/event.js
+++ js/event.js
@@ -1,1 +1,20 @@
-console.log("hi");
+// option 1: directly in the HTML as attribute
+
+// option 2: call function in attribute
+function makeRed() {
+	document.body.style.backgroundColor = "red";
+}
+
+// option 3: call function by id
+const blue = document.getElementById("make-blue");
+blue.onclick = makeBlue;
+
+function makeBlue() {
+	document.body.style.backgroundColor = "blue";
+}
+
+// option 4: call function by id shortcut
+const purple = document.getElementById("make-purple");
+purple.onclick = function purple() {
+	document.body.style.backgroundColor = "purple";
+};
